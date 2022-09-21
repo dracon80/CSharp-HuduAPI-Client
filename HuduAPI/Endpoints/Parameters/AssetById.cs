4a0ecb20962e904f5042f90ffcec6afda39b1463
dracon80 <nathan@dragonsandbears.id.au>
2022-09-21 10:22:49 +1000

    Commments added and converted field to property

--- HuduAPI/Endpoints/Parameters/AssetById.cs
+++ HuduAPI/Endpoints/Parameters/AssetById.cs
@@ -1,14 +1,15 @@
 ﻿namespace HuduAPI.Endpoints.Parameters
 {
     /// <summary>
-    /// Provides the paramaters to target a single Asset from the Hudu endpoint.
+    /// Provides the paramaters to target a single Asset from the Hudu endpoint. This is needed
+    /// because the Call tot he asset endpoint required additional information such as the company ID.
     /// </summary>
     public class AssetById : ItemById, IParameters
     {
         /// <summary>
         /// The companyID that the asset belongs too.
         /// </summary>
-        public int _companyId { get; set; }
+        private int _companyId { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AssetById" /> class.
@@ -22,6 +23,17 @@
         public AssetById(int id, int companyId) : base(id)
         {
             _companyId = companyId;
+        }
+
+        /// <summary>
+        /// Gets the company identifier.
+        /// </summary>
+        /// <value>
+        /// The company identifier.
+        /// </value>
+        public int CompanyId
+        {
+            get { return _companyId; }
         }
     }
 }
